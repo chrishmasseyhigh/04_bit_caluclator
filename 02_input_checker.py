f491ddcfa8437f80ebcc965e527deb6647c7c745
chrishmasseyhigh <87159815+chrishmasseyhigh@users.noreply.github.com>
2021-08-30 13:34:05 +1200

    else if

--- 02_input_checker.py
+++ 02_input_checker.py
@@ -1,15 +1,33 @@
 #checks user choice is 'interger', 'text' or 'image'
 def user_choice():
+    #list of valid responces
+    text_ok = [ "text","t","txt" ]
+    interger_ok = ["integer", "int", "#", "number"]
+    image_ok = ["image", "img", "pix", "picture", "pic"]
+
+
 
     valid = False
     while not valid:
         
         response = input("File type (interger / text / image): ").lower()
         
-        text_ok = [ "text","t","txt" ]
         if response in text_ok:
             return "text"
-         
+
+        elif response in interger_ok:
+            return "integer"
+
+        elif response in image_ok:
+            return "image" 
+        
+        elif response == "i":
+            want_integer = input("press <enter> for an interger or any key for a image")
+            if want_integer =="":
+                return "interger"
+            else:
+                return "image"
+        
         else:
             print()
             print("Please choose a valid file type!")
