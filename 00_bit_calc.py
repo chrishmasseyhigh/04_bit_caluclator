f7f565963927eab38d549910b91b627536766675
chrishmasseyhigh <87159815+chrishmasseyhigh@users.noreply.github.com>
2021-09-01 10:48:04 +1200

    Update 00_bit_calc.py

--- 00_bit_calc.py
+++ 00_bit_calc.py
@@ -16,5 +16,53 @@ def statement_generator(text, decoration):
 
     return ""
 
+# cheeks user choice is int, text or img
+def user_choice():
+    #list of valid responces
+    text_ok = [ "text","t","txt" ]
+    interger_ok = ["integer", "int", "#", "number"]
+    image_ok = ["image","p", "img", "pix", "picture", "pic",]
+
+
+
+    valid = False
+    while not valid:
+        
+        response = input("File type (interger / text / image): ").lower()
+        
+        if response in text_ok:
+            return "text"
+
+        elif response in interger_ok:
+            return "integer"
+
+        elif response in image_ok:
+            return "image" 
+        
+        elif response == "i":
+            want_integer = input("press <enter> for an interger or any key for a image")
+            if want_integer =="":
+                return "interger"
+            else:
+                return "image"
+        
+        else:
+            print()
+            print("Please choose a valid file type!")
+            print()
+
 # maine rotine goes here
 statement_generator("look stars", "*")
+
+#Heading
+statement_generator("Bit calculator for Integers, Text & Images", "-")
+
+#Display instructions if user has not used the program before
+
+# Loop to alow putiple calculations
+keep_going = ""
+while keep_going == "":
+    #ask the user for the file type
+    data_type= user_choice()
+    print("you chose", data_type)
+
