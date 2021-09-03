4d713e34f4336dc68e9ee222aa1b12d81cd1346d
chrishmasseyhigh <87159815+chrishmasseyhigh@users.noreply.github.com>
2021-09-03 14:43:57 +1200

    day 3

--- 03_interger_checker.py
+++ 03_interger_checker.py
@@ -3,9 +3,8 @@ def num_check(question, low):
     valid = False
     while not valid:
         
-        error = "please enter a integer that is more than"
-        "(or equal to) {}".format(low)
-
+        error = "please enter a integer that is more than (or equal to) {}".format(low)
+        error2= "please enter a integer that does not have decimal part / enter a number".format(low)
                                                                          
         try:
         
@@ -22,5 +21,22 @@ def num_check(question, low):
                 print()
                 
         except ValueError:
-            print(error)
+            print(error2)
 
+#Main rotine goes here
+
+keep_going = ""
+while keep_going == "":
+    print()
+
+     #Checks integer
+    var_integer=num_check("Please enter a integer: ", 0)
+    print()
+
+    #Checks width
+    width=num_check("please enter the width:", 1)
+    print()
+
+    #Checks hight
+    height=num_check("Please enter the hight:",1)
+    print()
